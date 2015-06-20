1ec54fb05fa5317c4e7c199eba7f4e3471959975
Konstantin Bogomolov <konstantin.bog@gmail.com>
2015-06-20 12:59:51 -0400

    What I said before

---
+++ window.lua
@@ -0,0 +1,23 @@
+maxWindowed = false
+maxFullscreen = false
+
+function makeWindow()
+	maxWidth, maxHeight = love.window.getDesktopDimensions( 1 )
+	local customWidth, customHeight = 1280, 720
+	
+	if maxWindowed then
+		love.window.setMode( maxWidth, maxHeight, 
+							 { borderless = true, centered = true, display = 1 } )
+	elseif maxFullscreen then
+		love.window.setMode( maxWidth, maxHeight, { fullscreen = true, display = 1 } )
+	else
+		love.window.setMode( customWidth, customHeight,
+							 { centered = true } )
+	end
+end
+
+
+function returnPlayerPosOnWindow() 
+	local width, height = love.window.getDimensions()
+	return width / 2, height / 2
+end
