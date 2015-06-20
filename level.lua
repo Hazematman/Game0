a38b15b6b8376f7a1bc928e1bb2ff14810f7796b
Konstantin Bogomolov <konstantin.bog@gmail.com>
2015-06-19 23:53:59 -0400

    Trying to add makeBuildings function. Functionality not quite there yet. Halp.

--- level.lua
+++ level.lua
@@ -1,22 +1,27 @@
+require("phys")
+
 Level = {}
 Level.__index = Level
+
+-- Constants
+til = 4 --tile size
+layerHeight = 10
+
 
 
 function Level.new( x, y, layers )
    local level = {}
    setmetatable( level, Level )
-
-   level.x = x or 100
-   level.y = y or 100
+   
+   level.x = x or 150
+   level.y = y or 200
    level.tiles = {}
    for i = 1, level.x * level.y do
-	  if 1 <= i and i <= level.x or level.y*(level.x-1) <= i and i <= level.y*level.x then
-		 level.tiles[ i ] = 1
-	  else
-		 level.tiles[ i ] = 0
-	  end
+	  level.tiles[ i ] = 0
    end
    level.layers = layers or 1
+
+   level.hitmap = HitMap.new()
    
    return level
 end
@@ -28,11 +33,47 @@ function Level:draw()
 		 cur_tile = self.y * (i - 1) + j
 		 if self.tiles[ cur_tile ] == 0 then
 			love.graphics.setColor( 84, 84, 84 ) 
-			love.graphics.rectangle("fill", 4 * (j - 1), 4 * (i - 1), 4, 4 )
+			love.graphics.rectangle("fill", til * (j - 1), til * (i - 1), til, til )
 		 elseif self.tiles[ cur_tile ] == 1 then
 			love.graphics.setColor ( 255, 255, 255 )
-			love.graphics.rectangle("fill", 4 * (j - 1), 4 * (i - 1), 4, 4 )
+			love.graphics.rectangle("fill", til * (j - 1), til * (i - 1), til, til )
 		 end
 	  end
    end
 end
+
+
+-- buildings array to pass in looks like { {y, y-size, x, x-size, layers}, ... }
+function Level:makeBuildings( buildings )
+   if buidlings == nil then 
+	  for i = 1, self.y do
+		 for j = 1, self.x do
+			if i == 1 or i == self.y
+			   or j == 1 or j == self.x
+			then 
+			   level.tiles[ (i - 1)*self.x + j ] = 1
+			   wall = HitBox.new( (j - 1)*til, (i - 1)*til, 0, j*til - 1, i*til - 1, 
+				  layerHeight * self.layers )
+			   self.hitmap:addBox( wall )				  
+			end
+		 end
+	  end
+   else
+	  for k,v in ipairs( buildings ) do
+		 for i = v[ 1 ], v[ 1 ]+v[ 2 ] do
+			for j = v[ 3 ], v[ 3 ]+v[ 4 ] do
+			   if i == v[ 1 ] or i == (v[ 1 ] + v[ 2 ] - 1)
+				  or j == v[ 3 ] or j == (v[ 3 ] + v[ 4 ] - 1)
+			   then  
+				  level.tiles[ (i - 1)*self.x + j ] = 1
+				  wall = HitBox.new( (j - 1)*til, (i - 1)*til, 0, j*til - 1, i*til - 1, 
+					 layerHeight * v[ 5 ] )
+				  self.hitmap:addBox( wall )
+			   end
+			end
+		 end
+	  end
+   end
+end
+		 
+		 
